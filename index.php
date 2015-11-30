c7b51b8b82ee128bcf54abbddc3a1148835a1504
yourwebcrafters <brad@yourwebcrafters.com>
2015-11-30 07:34:18 -0500

    Setup Continued
    Initial Project files send continued

---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Website Development Crafting SEO Promotion Creator Specialist</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link href='https://fonts.googleapis.com/css?family=Bangers|Russo+One|Lobster+Two|Courgette|Berkshire+Swash' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/ywc.css" />
+    <script src="js/vendor/modernizr.js"></script>
+
+  </head>
+  <body>
+    <div class="row full-width"><!-- Main Web Page Wrap Creation Start -->
+        <div id="topNavArea" class="large-12">
+
+        </div>
+        <div id="headerArea" class="large-12">
+          <div class="large-4 columns">
+              <div id="logoArea" class="large-12 background">
+
+              </div>
+          </div>
+          <div class="large-8 columns">
+
+          </div>
+        </div>
+        <div id="bottomNavArea" class="large-12">
+
+        </div>
+        <div id="mainContentArea" class="large-12">
+
+        </div>
+        <div id="footNavArea" class="large-12">
+
+        </div>
+        <div id="footerArea" class="large-12">
+
+        </div>
+        <div id="bottomTagLine" class="large-12">
+
+        </div>
+    </div><!-- Main Row Website Creator Stop Page End -->
+
+		<script src="js/vendor/jquery.js"></script>
+		<script src="js/foundation.min.js"></script>
+		<script>
+			$(document).foundation();
+		</script>
+	</body>
+</html>
